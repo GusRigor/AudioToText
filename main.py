ed204c73552e958eb713cda1cd1cfe23e763b4a9
Gustavo Rigor <gusrigor@icloud.com>
2020-11-05 10:10:04 -0300

    Adicionada uma maneira de salvar em txt o texto escutado

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import speech_recognition as sr
+
+print('Início')
 r = sr.Recognizer()
 print('Coloque o nome do arquivo (.wav)')
-path = input('')
-path = 'audio/' + path + '.wav'
+arquivo = input('')
+path = 'audio/' + arquivo + '.wav'
 print(path)
+print('pegou o arquivo')
 
 with sr.AudioFile(path) as source:
     audio = r.record(source)
+print('leu o arquivo')
+
 
 try:
     fala = r.recognize_google(audio,language='pt-BR')
@@ -16,3 +21,12 @@ except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+print('Gravando o arquivo em texto')    
+
+text_file = open("texto/" + arquivo + ".txt", "w")
+n = text_file.write(fala)
+text_file.close()
+
+print('Arquivo gravado')
+print('Fim!')
