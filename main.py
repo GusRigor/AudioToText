5991debcd473ab5ba02dec883466f906d64212ea
Gustavo Rigor <gusrigor@icloud.com>
2020-11-05 10:47:03 -0300

    Adicionada um estrutura legal para o programa

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 import speech_recognition as sr
 
+def ler_arquivo():
+    r = sr.Recognizer()
+    print('Coloque o nome do arquivo (.wav)')
+    arquivo = input('')
+    path = 'audio/' + arquivo + '.wav'
+    print(path)
+    return {'r':r, 'arquivo':arquivo, 'path':path}
+
+def gravar_audio(path, r):
+    with sr.AudioFile(path) as source:
+        audio = r.record(source)
+    return audio
+
+def converter_audio(r, audio):
+    try:
+        fala = r.recognize_google(audio,language='pt-BR')
+        print("Fala:")
+        print(fala)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return fala
+
+def salvar_texto(arquivo, fala):
+    text_file = open("texto/" + arquivo + ".txt", "w")
+    n = text_file.write(fala)
+    text_file.close()
+
+def main():
+    Arquivo = ler_arquivo()
+    print('pegou o arquivo')
+    Audio = gravar_audio(Arquivo['path'], Arquivo['r'])
+    print('leu o arquivo')
+    print('Convertendo o Arquivo...')
+    Fala = converter_audio(Arquivo['r'], Audio)
+    print('Gravando o arquivo em texto')    
+    salvar_texto(Arquivo['arquivo'], Fala)
+    print('Arquivo gravado')
+
 print('Início')
-r = sr.Recognizer()
-print('Coloque o nome do arquivo (.wav)')
-arquivo = input('')
-path = 'audio/' + arquivo + '.wav'
-print(path)
-print('pegou o arquivo')
+rodar_novamente = 'S'
+while rodar_novamente == 'S':
+    main()
+    print('Digite S para rodar novamente')
+    rodar_novamente = input('')
 
-with sr.AudioFile(path) as source:
-    audio = r.record(source)
-print('leu o arquivo')
-
-
-try:
-    fala = r.recognize_google(audio,language='pt-BR')
-    print("Fala:")
-    print(fala)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-print('Gravando o arquivo em texto')    
-
-text_file = open("texto/" + arquivo + ".txt", "w")
-n = text_file.write(fala)
-text_file.close()
-
-print('Arquivo gravado')
 print('Fim!')
